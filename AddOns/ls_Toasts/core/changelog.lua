b05ebfe3604536f60223b179a07bcd6658e54dcb
class2u <class2u@gmail.com>
2024-04-25 20:01:26 +0800

    更新 通知面板增強 30403.03

---
+++ AddOns/ls_Toasts/core/changelog.lua
@@ -0,0 +1,11 @@
+local _, addonTable = ...
+local E = addonTable.E
+
+-- Lua
+local _G = getfenv(0)
+
+-- Mine
+E.CHANGELOG = [[
+- Added 1.15.0 support.
+- Added Brazilian Portuguese translation. Translated by paulovnas@GitHub.
+]]
