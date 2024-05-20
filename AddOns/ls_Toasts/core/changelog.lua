96bef0e2dc0aed0ea80b1ede9b850e605c0a3479
class2u <class2u@gmail.com>
2024-05-20 13:57:38 +0800

    Add LS: Toasts 30403.03

---
+++ AddOns/ls_Toasts/core/changelog.lua
@@ -0,0 +1,10 @@
+local _, addonTable = ...
+local E = addonTable.E
+
+-- Lua
+local _G = getfenv(0)
+
+-- Mine
+E.CHANGELOG = [[
+- Added Brazilian Portuguese translation. Translated by paulovnas@GitHub.
+]]
