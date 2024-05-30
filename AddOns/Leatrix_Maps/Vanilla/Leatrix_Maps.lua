05c291ccca1a6378c0feb9a43a5a8907b0841150
class2u <class2u@gmail.com>
2024-05-30 08:16:11 +0800

    整合 世界地圖增強 1.15.33

--- AddOns/Leatrix_Maps/Vanilla/Leatrix_Maps.lua
+++ AddOns/Leatrix_Maps/Vanilla/Leatrix_Maps.lua
@@ -1,6 +1,6 @@
 ﻿
 	----------------------------------------------------------------------
-	-- 	Leatrix Maps 1.15.31 (15th May 2024)
+	-- 	Leatrix Maps 1.15.33 (29th May 2024)
 	----------------------------------------------------------------------
 
 	-- 10:Func, 20:Comm, 30:Evnt, 40:Panl
@@ -12,7 +12,7 @@
 	local LeaMapsLC, LeaMapsCB, LeaDropList, LeaConfigList, LeaLockList = {}, {}, {}, {}, {}
 
 	-- Version
-	LeaMapsLC["AddonVer"] = "1.15.31"
+	LeaMapsLC["AddonVer"] = "1.15.33"
 
 	-- Get locale table
 	local void, Leatrix_Maps = ...
